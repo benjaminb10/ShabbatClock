3c5c1688a0c79f2879712b2e9b478a0dde8952d9
Jimmy Douieb <jimmydouieb@gmail.com>
2016-11-05 13:36:28 +0200

    Separated Components

--- index.ios.js
+++ index.ios.js
@@ -14,48 +14,9 @@ import {
   View
 } from 'react-native';
 
-function getDayName(dateString) {
-  return ['DIM.', 'LUN.', 'MAR.', 'MER.', 'JEU.', 'VEN.', 'SAM.'][new Date(dateString).getDay()];
-}
+import Clock from './app/components/Clock';
 
-class Clock extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { date: new Date() };
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState({ date: new Date() });
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <FormattedClock date={this.state.date} />
-    );
-  }
-}
-
-class FormattedClock extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    let date = this.props.date;
-
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let time = (hours<10 ? '0'+hours : hours)+":"+(minutes<10 ? '0'+minutes : minutes)+":"+(seconds<10 ? '0'+seconds : seconds);
-    return (
-      <Text>
-        {getDayName(date)+"\n"+time}
-      </Text>
-    );
-  }
-}
 
 export default class ShabbatClock extends Component {
   render() {
