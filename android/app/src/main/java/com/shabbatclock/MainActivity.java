e1260568abc75bf4528b69e57c474ae2ae6ddd28
Jim Dou <jjj@jjj.com>
2016-11-14 11:24:31 +0200

    Replace all by ChabbatClock

--- android/app/src/main/java/com/shabbatclock/MainActivity.java
+++ android/app/src/main/java/com/shabbatclock/MainActivity.java
@@ -1,4 +1,4 @@
-package com.shabbatclock;
+package com.chabbatclock;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ShabbatClock";
+        return "ChabbatClock";
     }
 }
