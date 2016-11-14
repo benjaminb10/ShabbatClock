b02bef3ac917af6ac9376ae611ddbf58ab2f377b
Benjamin Benoudis <benoudis.benjamin@gmail.com>
2016-11-14 19:58:19 +0100

    Rename app to ShabbatWatch

--- android/app/src/main/java/com/shabbatwatch/MainActivity.java
+++ android/app/src/main/java/com/shabbatwatch/MainActivity.java
@@ -1,4 +1,4 @@
-package com.chabbatclock;
+package com.shabbatwatch;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ChabbatClock";
+        return "ShabbatWatch";
     }
 }
